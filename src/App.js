0474047bc6763d70b80764c11d1e9ec9d00e5bb0
Andrew Stern <andrewdstern16@gmail.com>
2022-03-18 14:04:27 -0600

    enter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  // state vars
+  const [heroes, setHeroes] = useState([])
+
+  const fetchHeroes = () => {
+    // console.log("click")
+    fetch("https://akabab.github.io/superhero-api/api/all.json")
+      .then(res => {
+        return res.json()
+      }).then(jsonRes => {
+        console.log(jsonRes);
+        setHeroes(jsonRes);
+      }).catch(someErr => console.log(someErr))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 🦸🦹Supper Heroes🦹🦸 </h1>
+      <h3>fetch / Axios / useEffect</h3>
+      <hr />
+      <button onClick={fetchHeroes}>fetch SuperHeroes</button>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Full Name</th>
+            <th>Images</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            heroes.map((hero, i) => {
+              return (
+                <tr key={hero.id}>
+                  <td>{hero.name}</td>
+                  <td>{hero.biography.fullName}</td>
+                  <td><img src={hero.images.xs} alt={`${hero.name}`}></img></td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
